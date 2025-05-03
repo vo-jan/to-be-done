32d06672cc141929b2e51e2806d4b85d69f6636d
vojan <55670438+vo-jan@users.noreply.github.com>
2025-05-03 22:11:17 +0200

    Fixes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.use(cors());
 app.use(express.json());
